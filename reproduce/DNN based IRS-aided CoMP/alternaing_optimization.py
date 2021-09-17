b919835136b46f186eaf2d579dd1cbbee3d558c8
dlwlrmaIU0516 <53083389+dlwlrmaIU0516@users.noreply.github.com>
2021-09-17 12:00:44 +0900

    upload

--- reproduce/DNN based IRS-aided CoMP/alternaing_optimization.py
+++ reproduce/DNN based IRS-aided CoMP/alternaing_optimization.py
@@ -11,9 +11,9 @@ def AO_cvx(p,H):
         H_bar = f_construct_H_bar(p,H,Phi)
         U = f_update_U(p,H_bar,W)
         Q = f_update_Q(p,H_bar,W,U)
-        W = f_update_W(p,H_bar,W,U,Q)
+        #W = f_update_W(p,H_bar,W,U,Q)
         Phi = f_update_Phi(p,H_bar,W,U,Q,H,Phi)
-        #min_rate = np.append(min_rate,f_min_rate(p,H,W,U,Q,Phi))
+        min_rate = np.append(min_rate,f_min_rate(p,H,W,U,Q,Phi))
         if np.size(min_rate)>2 and np.abs(min_rate[-1]-min_rate[-2])<1e-3:
             break
     return min_rate
