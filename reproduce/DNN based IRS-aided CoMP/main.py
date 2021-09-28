e8cb30f73a436bb533e3c6178735343fa9d5c214
dlwlrmaIU0516 <53083389+dlwlrmaIU0516@users.noreply.github.com>
2021-09-28 09:53:28 +0900

    uplaod

--- reproduce/DNN based IRS-aided CoMP/main.py
+++ reproduce/DNN based IRS-aided CoMP/main.py
@@ -24,17 +24,10 @@ cuda = torch.device('cuda')
 print(cuda)
 
 # GPU 할당 변경하기
-GPU_NUM = 0 # 원하는 GPU 번호 입력
-device = torch.device('cuda:{GPU_NUM}' if torch.cuda.is_available() else 'cpu')
-torch.cuda.set_device(device) # change allocation of current GPU
-print ('Current cuda device ', torch.cuda.current_device()) # check
-
-# Additional Infos
-if device.type == 'cuda':
-    print(torch.cuda.get_device_name(GPU_NUM))
-    print('Memory Usage:')
-    print('Allocated:', round(torch.cuda.memory_allocated(GPU_NUM)/1024**3,1), 'GB')
-    print('Cached:   ', round(torch.cuda.memory_cached(GPU_NUM)/1024**3,1), 'GB')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print ('Available devices ', torch.cuda.device_count())
+print ('Current cuda device ', torch.cuda.current_device())
+print(torch.cuda.get_device_name(device))
 
     
 NN_CoMP = IRS_CoMP_Net(p['N_t'],p['N_r'],p['M'],p['num_BS'],p['K'],p['d'],p['batch_size'],p['Tx_P'])
